36ba50269b22d8f517c3656a218bd87b5bbbf32e
Darnelle Melvin <32146321+darnelleMelvin@users.noreply.github.com>
2024-04-25 14:42:11 -0700

    Add files via upload
    Version 1.1 accounts for rdf literals containing double-quotes ("). The python script will escape the characters upon output.

---
+++ csv2rdfConverter_v1.1.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 25 14:29:53 2024
+
+@author: dmelvin
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 11:38:33 2024
+@author: dmelvin
+"""
+
+import csv
+import os
+
+def format_triple(subject, predicate, obj):
+    # Check if object is a URI or literal
+    if obj.startswith("http"):
+        return f"<{subject}> <{predicate}> <{obj}> ."
+    else:
+        # Escape single and double quotes within the object string
+        obj = obj.replace('"', '\\"')
+        # Specify English language tag
+        return f"<{subject}> <{predicate}> \"{obj}\"@en ."
+
+def convert_csv_to_nt(input_csv, output_nt):
+    with open(input_csv, 'r', newline='', encoding='utf-8') as csvfile, open(output_nt, 'w', encoding='utf-8') as ntfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)  # Skip the header row
+        for row in csvreader:
+            if len(row) >= 3:
+                subject, predicate, obj, *_ = row
+                nt_triple = format_triple(subject, predicate, obj)
+                ntfile.write(nt_triple + '\n')
+            else:
+                print("Error: Invalid row format - should contain at least 3 columns")
+
+if __name__ == "__main__":
+    input_csv_file = "C:/Users/dmelvin/Downloads/Wikidata_funkDataSet4.csv"  # Change this to the path of your input CSV file
+    output_nt_file = "C:/Users/dmelvin/Documents/graphBD_test/rdf_batch/output2.nt"  # Change this to the desired path of your output .nt file
+    
+    convert_csv_to_nt(input_csv_file, output_nt_file)
