1cc69d5f2c566418b4838da9124f8c38488ff3ef
Darnelle Melvin <32146321+darnelleMelvin@users.noreply.github.com>
2024-12-20 11:01:47 -0800

    this update fixes error when handeling iso dates. In previous version dates were tagged with an en language tag

--- csv2rdfConverter.py
+++ csv2rdfConverter.py
@@ -1,20 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr  5 11:38:33 2024
-@author: dmelvin
-"""
-
 import csv
+from dateutil import parser
+
 
 def format_triple(subject, predicate, obj):
-    # Check if object is a URI or literal
+    # Check if object is a URI
     if obj.startswith("http"):
         return f"<{subject}> <{predicate}> <{obj}> ."
     else:
-        # Escape double quotes within the object string
-        obj = obj.replace('"', '\\"')
-        # Specify English language tag
-        return f"<{subject}> <{predicate}> \"{obj}\"@en ."
+        # Check if object is a datetime
+        try:
+            parsed_date = parser.isoparse(obj)
+            # Format as xsd:dateTime
+            return f"<{subject}> <{predicate}> \"{parsed_date.isoformat()}\"^^<http://www.w3.org/2001/XMLSchema#dateTime> ."
+        except ValueError:
+            # Escape single and double quotes within the object string
+            obj = obj.replace('"', '\\"')
+            # Specify English language tag
+            return f"<{subject}> <{predicate}> \"{obj}\"@en ."
+
 
 def convert_csv_to_nt(input_csv, output_nt):
     with open(input_csv, 'r', newline='', encoding='utf-8') as csvfile, open(output_nt, 'w', encoding='utf-8') as ntfile:
@@ -23,13 +26,17 @@ def convert_csv_to_nt(input_csv, output_nt):
         for row in csvreader:
             if len(row) >= 3:
                 subject, predicate, obj, *_ = row
-                nt_triple = format_triple(subject, predicate, obj)
-                ntfile.write(nt_triple + '\n')
+                try:
+                    nt_triple = format_triple(subject, predicate, obj)
+                    ntfile.write(nt_triple + '\n')
+                except Exception as e:
+                    print(f"Error processing row {row}: {e}")
             else:
-                print("Error: Invalid row format - should contain at least 3 columns")
+                print(f"Error: Invalid row format - should contain at least 3 columns. Row: {row}")
+
 
 if __name__ == "__main__":
-    input_csv_file = "FILE_PATH_TO_INPUT.csv"  # Change this to the path of your input CSV file
-    output_nt_file = "FILE_PATH_TO_OUTPUT.nt"  # Change this to the desired path of your output .nt file
-    
+    input_csv_file = "inputFilepathHere.csv"  # Change this to the path of your input CSV file
+    output_nt_file = "outputFilepathHere.nt"  # Change this to the desired path of your output .nt file
+
     convert_csv_to_nt(input_csv_file, output_nt_file)
